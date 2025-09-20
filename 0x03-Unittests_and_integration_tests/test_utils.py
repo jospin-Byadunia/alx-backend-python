db442bece036f02306b0bc82554474bdf6fe5cde
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-20 21:30:48 +0200

    Add utility functions and unit tests for nested map access

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,16 @@
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+# nested_map={"a": 1}, path=("a",)
+# nested_map={"a": {"b": 2}}, path=("a",)
+# nested_map={"a": {"b": 2}}, path=("a", "b")
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
