ad8c5d44f16207c9f8d95c07b327cd8d9f03ffdf
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-21 22:43:07 +0200

    Add DRF authentication URLs to the main URL configuration

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # include chats app URLs
+     path('api-auth/', include('rest_framework.urls')),  # DRF login/logout
 ]
