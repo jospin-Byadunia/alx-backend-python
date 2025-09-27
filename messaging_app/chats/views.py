fa00f841ebfa59a255434bb26b371cb2fa75b448
jospin Byadunia <jmurhiorhakube@gmail.com>
2025-09-27 11:01:08 +0200

    Refactored the views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -3,7 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Conversation, Message, User
 from .serializers import ConversationSerializer, MessageSerializer
-from .permissions import IsParticipantOfConversation, IsMessageOwner
+from .permissions import IsParticipantOfConversation
+
 
 class ConversationViewSet(viewsets.ModelViewSet):
     """
@@ -12,10 +13,12 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all().prefetch_related('participants', 'messages')
     serializer_class = ConversationSerializer
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
-
-    # Adding filtering capability
     filter_backends = [filters.SearchFilter]
     search_fields = ['participants__first_name', 'participants__last_name', 'participants__email']
+
+    def get_queryset(self):
+        # Restrict list to conversations where the user is a participant
+        return self.queryset.filter(participants=self.request.user)
 
     def create(self, request, *args, **kwargs):
         participants_ids = request.data.get('participants', [])
@@ -24,6 +27,7 @@ class ConversationViewSet(viewsets.ModelViewSet):
                 {"error": "A conversation must have at least 2 participants."},
                 status=status.HTTP_400_BAD_REQUEST
             )
+
         participants = User.objects.filter(id__in=participants_ids)
         conversation = Conversation.objects.create()
         conversation.participants.set(participants)
@@ -37,10 +41,13 @@ class MessageViewSet(viewsets.ModelViewSet):
     """
     queryset = Message.objects.all().select_related('sender', 'conversation')
     serializer_class = MessageSerializer
-    permission_classes = [IsParticipantOfConversation]
-
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
     filter_backends = [filters.SearchFilter]
     search_fields = ['message_body']
+
+    def get_queryset(self):
+        # Restrict list to messages in conversations the user participates in
+        return self.queryset.filter(conversation__participants=self.request.user)
 
     def create(self, request, *args, **kwargs):
         sender = request.user
@@ -59,6 +66,13 @@ class MessageViewSet(viewsets.ModelViewSet):
             return Response(
                 {"error": "Conversation does not exist"},
                 status=status.HTTP_404_NOT_FOUND
+            )
+
+        # Check if sender is a participant in the conversation
+        if sender not in conversation.participants.all():
+            return Response(
+                {"error": "You are not a participant in this conversation."},
+                status=status.HTTP_403_FORBIDDEN
             )
 
         message = Message.objects.create(
